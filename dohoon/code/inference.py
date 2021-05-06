634fd7b316e2228fdd9f25572ee5d1cc28328217
vail131 <vail131@naver.com>
2021-05-06 05:30:27 +0000

    whole-in-one code upload

--- dohoon/code/inference.py
+++ dohoon/code/inference.py
@@ -98,9 +98,9 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default=None)
-    parser.add_argument("--model_dir", type=str, default=None)
-    parser.add_argument("--output_dir", type=str, default=None)
+    parser.add_argument("--data_dir", type=str, default='/opt/ml/input/data/eval_dataset')
+    parser.add_argument("--model_dir", type=str, default='/opt/ml/p3-dst-dts/dohoon/code/model/model-best.bin')
+    parser.add_argument("--output_dir", type=str, default='/opt/ml/p3-dst-dts/dohoon/code/predictions')
     parser.add_argument("--eval_batch_size", type=int, default=32)
     args = parser.parse_args()
     
---
+++ dohoon/code/whole-in-one.py
@@ -0,0 +1,20 @@
+import time
+from submit import submit
+if __name__ == "__main__":
+    start = time.time()  # 시작 시간 저장
+    import train_copy
+    print("학습시간 : ", time.time()-start)
+    import inference
+    print("추론시간 : ", time.time()-start)
+
+
+    test_dir='/opt/ml/p3-dst-dts/dohoon/code/predictions'
+
+    #아래 글을 통해 자신의 key값 찾아 넣기
+    #http://boostcamp.stages.ai/competitions/3/discussion/post/110
+    # submit("Bearer 15bdf505e0902975b2e6f578148d22136b2f7717", os.path.join(test_dir, 'predictions.csv'))
+
+    print("전체시간 :", time.time() - start)  
+
+ 
+ 
