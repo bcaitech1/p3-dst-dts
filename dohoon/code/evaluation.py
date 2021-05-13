a91e5f622e035bb2efd24f33f06f9fd5aeded9af
vail131 <vail131@naver.com>
2021-05-13 02:33:26 +0000

    eda

--- dohoon/code/evaluation.py
+++ dohoon/code/evaluation.py
@@ -1,7 +1,7 @@
 import json
 import argparse
 from eval_utils import DSTEvaluator
-
+from data_utils import split_slot
 
 SLOT_META_PATH = 'data/train_dataset/slot_meta.json'
 
@@ -14,16 +14,44 @@ def _evaluation(preds, labels, slot_meta):
     
     wrong_list=[]
     correct_list=[]
+    guid_compare_dict=dict(list)
     for k, l in labels.items():
+        # k는 guid, l은 dialogue state
         p = preds.get(k)
         if p is None:
             raise Exception(f"{k} is not in the predictions!")
         evaluator.update(l, p)
+        
+        if "지하철" in k or "택시" in k and set(l)!=set(p):
+            #맞춘 부분을 제외한 모든 dom-slot-value
+            input_list=[]
+            for val in ((set(l) | set(p)) - (set(l) & set(p))):
+                dom_slot,value=split_slot(val, get_domain_slot=True)
+                #모델이 gt의 slot을 무시했을 경우 
+                if dom_slot in set(l) and dom_slot not in set(p):
+                    gt=value
+                    pr='none'
+                #모델이 gt에 없는 이상한 slot을 예측하여 value를 넣은 경우
+                elif dom_slot in set(p) and dom_slot not in set(l) :
+                    gt='none'
+                    pr=value
+                #모델이 gt에 존재하는 slot의 value를 잘못 예측했을 경우
+                else :
+                    pr=value
+                    for l_val in set(l):
+                        _,value_gt=split_slot(l_val, get_domain_slot=True)
+                        if dom_slot in l_val:
+                            gt=value_gt
+                            break
+                    else :
+                        gt='something_wrong'
+                input_list.extend(f"{dom_slot}-{gt}/{pr}")    
+            guid_compare_dict.get(k)=input_list
         wrong_list.extend(set(l)-set(p))
         correct_list.extend(set(l))
     result = evaluator.compute()
     print(result)
-    return result,wrong_list,correct_list
+    return result,wrong_list,correct_list,guid_compare_dict
 
 
 def evaluation(gt_path, pred_path):
